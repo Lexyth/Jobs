fc2b4630ef0c1ea750a83b59ca647680b8f254db
Lexyth <marfy@web.de>
2024-05-14 01:54:21 +0200

    feat(clients): Add ClientsHandler class
    Added a utility class which facilitates interacting with the clients data structure

---
+++ src/components/Clients/clientsHandler.ts
@@ -0,0 +1,69 @@
+import { clientsStore } from "./store";
+
+import type { Client } from "./store";
+
+export class ClientsHandler {
+    static #instance: ClientsHandler | null = null;
+
+    constructor() {
+        if (ClientsHandler.#instance) {
+            return ClientsHandler.#instance;
+        } else {
+            ClientsHandler.#instance = this;
+        }
+    }
+
+    get loaded(): boolean {
+        return clientsStore.loaded;
+    }
+
+    addClient(client: Client): number {
+        client.id = clientsStore.get().reduce((id, client) => client.id > id ? client.id : id, 0) + 1;
+        const newClients = [...clientsStore.get(), { ...client }];
+        clientsStore.set(newClients);
+        return client.id;
+    };
+
+    removeClient(client: Client | number): boolean {
+        let newClients;
+        if (client instanceof Object) {
+            newClients = clientsStore.get().filter((oldClient) => oldClient.id !== client.id);
+        } else {
+            newClients = clientsStore.get().filter((oldClient) => oldClient.id !== client);
+        }
+        clientsStore.set(newClients);
+        return newClients.length !== clientsStore.get().length;
+    }
+
+    setClient(client: Client): boolean {
+        const index = clientsStore.get().findIndex((oldClient) => oldClient.id === client.id);
+
+        if (index === -1) {
+            return false;
+        } else {
+            clientsStore.set([
+                ...clientsStore.get().slice(0, index),
+                { ...client },
+                ...clientsStore.get().slice(index + 1)
+            ]);
+            return true;
+        }
+    }
+
+    getClient(identifier: string | number): Client | void {
+        let client: Client | undefined;
+        if (typeof identifier === "string") {
+            client = clientsStore.get().find((client) => client.name === identifier);
+        } else {
+            client = clientsStore.get().find((client) => client.id === identifier);
+        }
+
+        if (client === undefined)
+            return;
+        return { ...client };
+    }
+
+    getClients(): Client[] {
+        return [...clientsStore.get()];
+    }
+}
