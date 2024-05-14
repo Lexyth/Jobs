9381c2c94cbbe02d95e28984c690e26365f22241
Lexyth <marfy@web.de>
2024-05-14 03:12:29 +0200

    feat(utils): Add PeristentStore and PersistentCSVStore
    Added createPersistentStore and createPersistentCSVStore to create stores that can load and save their value using the persistence api. The Persistente CSV Store also converts its array value to and from a comma separated values string when saving and loading, respectively.

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,8 +1,25 @@
+import { load, save } from "./persistence";
 
-// annotate the type that is returned and dont use any
+export type Store<Type> = {
+    get: () => Type;
+    set: (newValue: Type) => void;
+    subscribe: (listener: () => void) => () => void;
+    getSnapshot: () => Type;
+}
+
+export type PersistentStore<Type> = (
+    Store<Type>
+    & {
+        loaded: boolean,
+        isSaved: boolean,
+        save: () => Promise<boolean>,
+        load: () => Promise<Type>
+    }
+);
+
 export function createStore<Type>(
     initialValue: Type
-) {
+): Store<Type> {
     let value: Type = initialValue;
 
     let listeners: (() => void)[] = [];
@@ -17,7 +34,7 @@ export function createStore<Type>(
         return () => unsubscribe(listener);
     }
 
-    function getSnapshot() {
+    function getSnapshot(): Type {
         return value;
     }
 
@@ -25,10 +42,104 @@ export function createStore<Type>(
         listeners.forEach((listener) => { listener(); });
     }
 
-    return {
-        get() { return value; },
-        set(newValue: Type) { value = newValue; emit(); },
+    function get(): Type {
+        return value;
+    }
+
+    function set(newValue: Type): void {
+        value = newValue;
+        emit();
+    }
+
+    const store: Store<Type> = {
+        get,
+        set,
         subscribe,
         getSnapshot
     }
+
+    return store;
+}
+
+export function createPersistentStore<Type>(
+    initialValue: Type,
+    load: () => Promise<Type>,
+    save: (snapshot: Type) => Promise<boolean>,
+    saveDelay: number = 10000
+): PersistentStore<Type> {
+    const store = createStore(initialValue) as PersistentStore<Type>;
+
+    store.loaded = false;
+
+    store.isSaved = true;
+
+    let saveDelayTimeoutId: number | undefined;
+
+    const set = store.set;
+    store.set = (newValue: Type) => {
+        store.isSaved = false;
+        set(newValue);
+        clearTimeout(saveDelayTimeoutId);
+        saveDelayTimeoutId = setTimeout(() => {
+            if (store.loaded && store.isSaved === false) {
+                store.save();
+            }
+        }, saveDelay);
+    };
+
+    store.load = async () => {
+        store.loaded = false;
+        const result = await load();
+        store.loaded = true;
+        store.set(result);
+        store.isSaved = true;
+        return result;
+    };
+
+    store.save = async () => {
+        store.isSaved = true;
+        clearTimeout(saveDelayTimeoutId);
+        const result = await save(store.getSnapshot());
+        return result;
+    }
+
+    store.load();
+
+    return store;
+}
+
+export function createPersistentCSVStore<Type>(
+    initialValue: Type[],
+    path: string,
+    toArray: (obj: Type) => string[],
+    fromArray: (arr: string[]) => Type,
+    saveDelay?: number
+): PersistentStore<Type[]> {
+    const loadCSV = async () => {
+        const text = await load(path);
+        if (!text) {
+            console.warn("Loading CSV failed. No text in file.");
+            return initialValue;
+        }
+
+        const dataArray = text.split("\n");
+
+        let result: Type[] = [];
+
+        for (let i = 0; i < dataArray.length; i++) {
+            const data = dataArray[i].split(",");
+
+            const obj = fromArray(data);
+            result.push(obj);
+        }
+
+        return result;
+    };
+    const saveCSV = async (snapshot: Type[]) => {
+        const csv: string = snapshot.map((obj) => toArray(obj).join(",")).join("\n");
+        const result: boolean = await save(path, csv, "text/csv");
+
+        return result;
+    };
+    return createPersistentStore(initialValue, loadCSV, saveCSV, saveDelay);
 }
