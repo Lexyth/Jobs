8d19708abe33213d86ceed279bae86fffbf619eb
Lexyth <marfy@web.de>
2024-05-14 02:20:24 +0200

    feat(persistence): Add persistence functions
    Added more general persistence functionality to act as an API to integrate other persistence systems in the future

---
+++ src/utils/persistence.ts
@@ -0,0 +1,17 @@
+import { download, upload } from "./dropbox";
+
+// TODO: implement local storage with file system access API
+
+export function load(path: string) {
+    const file = download(path);
+
+    const textPromise = file.then(file => {
+        return file?.text()
+    });
+    return textPromise;
+}
+
+export function save(path: string, content: string, type: string = "text/plain") {
+    const file = new File([content], path, { type });
+    return upload(file);
+}
