9b8a45328dcf9f0390c92f45a88cf83d4821bc97
Lexyth <marfy@web.de>
2024-05-30 23:58:39 +0200

    refactor(clients): Merged useClients with useClientsHandler
    Merged the useClients hook into useClientsHandler, since client data should only be accessed via the client handler.

--- src/components/Clients/hook.ts
+++ src/components/Clients/hook.ts
@@ -3,19 +3,7 @@ import React from "react";
 import { clientsStore } from "./store";
 import { ClientsHandler } from "./clientsHandler";
 
-// TODO: useClients should become obsolete and ClientsHandler should expose subscribe and getSnapshot methods; maybe get() suffices if ClientsHandler can guarantee immutability
-
-export function useClients() {
-  const clients = React.useSyncExternalStore(
-    clientsStore.subscribe,
-    clientsStore.getSnapshot
-  );
-
-  return clients;
-}
-
 export function useClientsHandler() {
-  useClients();
-
+  React.useSyncExternalStore(clientsStore.subscribe, clientsStore.getSnapshot);
   return new ClientsHandler();
 }
