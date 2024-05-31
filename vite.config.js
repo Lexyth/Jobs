1e1a5cd18b9a10fa0dcbf7cc6054c76396be72a2
Lexyth <marfy@web.de>
2024-05-31 05:50:15 +0200

    build(eslint): Add type checker
    Added type checker.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import eslint from "vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,8 +17,15 @@ export default defineConfig({
       cache: false,
       failOnError: true,
       failOnWarning: true,
-      include: ["src/**/*.ts", "src/**/*.tsx", "../**/*.ts", "../**/*.tsx"],
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: [
+        "src/**/*.test.ts",
+        "src/**/*.test.tsx",
+        "**dist/**",
+        "**.d.ts",
+      ],
     }),
+    checker({ typescript: true }),
   ],
   server: {
     open: true,
