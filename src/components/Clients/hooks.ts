680f25dc922635044a5ce6258a0738c286a6dffa
Lexyth <marfy@web.de>
2024-06-08 07:25:33 +0200

    refactor(clients): Add entries to Editor
    Added the following entries to the clients editor:
    - company
    - address
    - zip
    - city
    - country
    - UST-ID-Nr.
    - Bestell-Nr.
    - Zahlungsbedingungen
    - Liefer-Nr.
    - Nr.

--- src/components/Clients/hooks.ts
+++ src/components/Clients/hooks.ts
@@ -15,6 +15,34 @@ export function useHandler() {
 export function useEntryMap(client: Client) {
   const entryMap = {
     name: useEntry("Name", "input", [{ current: true, value: client.name }]),
+    company: useEntry("Company", "input", [
+      { current: true, value: client.company || "" },
+    ]),
+    address: useEntry("Address", "input", [
+      { current: true, value: client.address || "" },
+    ]),
+    zip: useEntry("Zip", "input", [{ current: true, value: client.zip || "" }]),
+    city: useEntry("City", "input", [
+      { current: true, value: client.city || "" },
+    ]),
+    country: useEntry("Country", "input", [
+      { current: true, value: client.country || "" },
+    ]),
+    "UST-ID-Nr.": useEntry("UST-ID-Nr.", "input", [
+      { current: true, value: client["UST-ID-Nr."] || "" },
+    ]),
+    "Bestell-Nr.": useEntry("Bestell-Nr.", "input", [
+      { current: true, value: client["Bestell-Nr."] || "" },
+    ]),
+    Zahlungsbedingungen: useEntry("Zahlungsbedingungen", "input", [
+      { current: true, value: client.Zahlungsbedingungen || "" },
+    ]),
+    "Liefer-Nr.": useEntry("Liefer-Nr.", "input", [
+      { current: true, value: client["Liefer-Nr."] || "" },
+    ]),
+    "Nr.": useEntry("Nr.", "input", [
+      { current: true, value: client["Nr."] || "" },
+    ]),
   };
   return entryMap;
 }
