2d274f57579a63633ea3762fec04edf819c52ce3
Lexyth <marfy@web.de>
2024-06-08 07:23:29 +0200

    refactor(clients): Remove toEntryDataMap
    Removed toEntryDataMap function from scripts.

--- src/components/Clients/script.ts
+++ src/components/Clients/script.ts
@@ -1,6 +1,5 @@
 import type { Client } from "./store.js";
 import type { SummaryDataWithAttrs } from "../../utils/components/List/react.js";
-import type { EntryValueMap } from "../../utils/components/Editor/react.js";
 
 export function toSummaryData(
   client: Client,
@@ -15,10 +14,4 @@ export function toSummaryData(
   }
 
   return summary;
-}
-
-export function toEntryDataMap(client: Client): EntryValueMap {
-  return {
-    name: client.name,
-  };
 }
